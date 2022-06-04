3a8521574bd8408a07414790bf967e185098b553
Evans Rutto <robbyevans001@gmail.com>
2022-06-05 01:03:20 +0300

    commit tree 0c66619ef65ff7904d499a0cd39cea5c1459fbaf

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee={
+    name:'robby',
+    streetAddress:'7528-Eldoret',
+}
+
+function  updateEmployeeWithKeyAndValue(employee,key,value){
+    return{...employee,[key]:value,}
+}
+updateEmployeeWithKeyAndValue(employee,'name','Sam')
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee,'streetAddress', '11 Broadway')
+
+function deleteFromEmployeeByKey(employee,key){
+    let newEmployee={...employee,};
+    delete newEmployee[key];
+    return newEmployee;
+}
+deleteFromEmployeeByKey(employee,'name');
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee,'name');
